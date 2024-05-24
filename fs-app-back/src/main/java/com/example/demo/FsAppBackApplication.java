5709115e6e35f8eaaf165d480d4dd4b6cba10810
taisuke.haga <taisuke.haga@172.16.2.28>
2024-05-24 15:07:02 +0900

    SpringBootの新規プロジェクトを作成

---
+++ fs-app-back/src/main/java/com/example/demo/FsAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsAppBackApplication.class, args);
+	}
+
+}
