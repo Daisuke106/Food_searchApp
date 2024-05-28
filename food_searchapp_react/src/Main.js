57d41d89e5377e8d26a88a09b4d4ec4ef3b1aa83
Daisuke <>
2024-05-28 18:31:50 +0900

    画像にスライドショー機能を追加＆マップ表示

--- food_searchapp_react/src/Main.js
+++ food_searchapp_react/src/Main.js
@@ -3,13 +3,16 @@ import { Box, Button } from '@yamada-ui/react';
 import { LoadScript, GoogleMap } from "@react-google-maps/api";
 import Slider from "react-slick";
 import { useNotice } from '@yamada-ui/react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Main.css';
+
 
 function Main() {
     const [position, setPosition] = useState({ lat: 35.680959106959, lng: 139.76730676352 });
     const notice = useNotice();
     const noticeRef = useRef();
-    // 画像パスをルートからの相対パスに変更
-    const datas = [{ url: "/images/family_food.jpg", name: "店舗1" }, { url: "/images/image2.jpg", name: "店舗2" }];
+    const datas = [{ url: "/images/family_food.jpg", name: "店舗1" }, { url: "/images/sample_food.jpg", name: "店舗2" }];
 
     const handleLocation = () => {
         noticeRef.current = notice({
@@ -46,23 +49,25 @@ function Main() {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
     };
 
     return (
-        <Box className="container" display="flex" flexDirection="column" height="100vh">
-            <Box className="header" width="100%" zIndex="1000" position="fixed" top="0" backgroundColor="white">
-                <Slider {...sliderSettings}>
+        <Box className="container" display="flex" flexDirection="column" height="100vh" id='MainCS'>
+            <Box className="header2" width="100%" zIndex="1" position="fixed" top="0" backgroundColor="white">
+                <Slider {...sliderSettings} style={{ height: "500px" }}>
                     {datas.map(data => (
                         <div key={data.url}>
-                            <img src={data.url} alt={data.name} style={{ width: "100%", height: "auto" }} />
+                            <img src={data.url} alt={data.name} style={{ width: "100%", height: "500px", objectFit: "cover" }} />
                         </div>
                     ))}
                 </Slider>
             </Box>
-            <Box display="flex" flexDirection="column" height="100vh" paddingTop="300px">
+            <Box display="flex" flexDirection="column" paddingTop="360px">
                 <Button onClick={handleLocation} colorScheme="blue">現在地の取得</Button>
-                <Box width="100vw" height="50vh" marginTop="20px">
+                <Box width="80vw" height="50vh" marginTop="20px">
                     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
                         <GoogleMap mapContainerStyle={{ width: "100%", height: "100%" }} center={position} zoom={15}>
                         </GoogleMap>
